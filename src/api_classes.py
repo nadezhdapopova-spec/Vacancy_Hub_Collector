a7c5ab9a604bf28c4921d41e2d2c3d6a563fc846
Nadezhda_Popova <nadezhdapopova13@yandex.ru>
2025-08-01 22:08:30 +0700

    modified src/api_classes, src/class_vacancy

--- src/api_classes.py
+++ src/api_classes.py
@@ -4,6 +4,8 @@ from typing import Any, Union
 import requests
 from requests import Response
 
+from src.class_vacancy import Vacancy
+
 
 class BaseVacanciesSource(ABC):
     """Абстрактный класс для получения данных через API по ключевому слову"""
@@ -14,48 +16,73 @@ class BaseVacanciesSource(ABC):
         pass
 
     @abstractmethod
-    def get_data(self, key_word: str) -> Any:
-        "Обрабатывает GET-запрос и полученные данные о вакансиях"
+    def get_vacancies_data(self, key_word: str) -> Any:
+        "Обрабатывает GET-запрос и получает данные о вакансиях"
         pass
 
 
 class HeadHunterVacanciesSource(BaseVacanciesSource):
     """Класс для получения через API данных сайта HeadHunter.ru о вакансиях по ключевому слову"""
 
-
     __slots__ = ("__url", "__headers", "__params", "__vacancies")
     __url: str
     __headers: dict
     __params: dict
-    __vacancies: list
+    # _vacancies: list[Vacancy]
 
     def __init__(self) -> None:
-        "Конструктор для получения вакансий через API"
+        """Конструктор для получения вакансий через API"""
         self.__url = "https://api.hh.ru/vacancies"
         self.__headers = {"User-Agent": "api-test-agent"}
         self.__params = {"text": "", "per_page": 50, "only_with_salary": True}
-        self.__vacancies = []
+        # self._vacancies = []
         super().__init__()
 
+    # @property
+    # def vacancies(self) -> list[Vacancy]:
+    #     return self._vacancies.copy()
+
     def _connect(self) -> Response | None:
-        "Делает GET-запрос, проверяет статус-код ответа"
-        try:
-            response = requests.get(self.__url, headers=self.__headers, params=self.__params)
-            if response.status_code != 200:
-                return None
-            return response
-        except Exception as err:
-            print(err)
+        """Делает GET-запрос, проверяет статус-код ответа"""
+        response = requests.get(self.__url, headers=self.__headers)
+        if response.status_code != 200:
+            print("Ошибка подключения")
             return None
+        return response
 
-
-    def get_data(self, key_word: str) -> Any:
-        "Обрабатывает GET-запрос и полученные данные о вакансиях"
+    def get_vacancies_data(self, key_word: str) -> Any:
+        "Обрабатывает GET-запрос и получает данные о вакансиях"
         self.__params["text"] = key_word
-        try:
-            data = self._connect()
-            vacancies = data.json()["items"]
-        except Exception as err:
-            print(err)
-        else:
-            return self.__vacancies
+        if self._connect():
+            try:
+                response = requests.get(self.__url, headers=self.__headers, params=self.__params)
+                return response.json().get("items", [])
+            except Exception as err:
+                print(err)
+        return []
+
+    def get_vacancies(self, key_word: str) -> list[Vacancy]:
+        """Получает данные о вакансиях и возвращает список объектов Vacancy"""
+        # vac_data = self.get_vacancies_data(key_word)
+        # self._vacancies = self.format_vacancies(vac_data)
+        # return self._vacancies
+        vacancies_data = self.get_vacancies_data(key_word)
+        return self.format_vacancies(vacancies_data)
+
+    @staticmethod
+    def format_vacancies(vacancies_data: dict) -> list[Vacancy]:
+        """Формирует список объектов Vacancy"""
+        return [
+            Vacancy(
+                id=vac.get("id"),
+                name=vac.get("name"),
+                url=vac.get("alternate_url"),
+                salary_from=vac.get("salary", {}).get("from"),
+                salary_to=vac.get("salary", {}).get("to"),
+                salary_mode=vac.get("salary_range", {}).get("mode", {}).get("id"),
+                employer_name=vac.get("employer", {}).get("name"),
+                employer_url=vac.get("employer", {}).get("alternate_url"),
+                requirements=vac.get("snippet", {}).get("requirement"),
+                area=vac.get("area", {}).get("name")
+            )
+            for vac in vacancies_data]
--- src/class_vacancy.py
+++ src/class_vacancy.py
@@ -2,13 +2,41 @@ from typing import Union
 
 
 class Vacancy:
-    "Класс для создания вакансий"
-    id: Union[str, int]
-    name: str
-    url: str
-    description: str
-    salary: Union[int, float]
-    requirements: list[str]
+    "Класс для создания вакансии"
+    __slots__ = ("id", "name", "url", "salary_from", "salary_to", "salary_mode",
+                 "employer_name", "employer_url", "requirements", "area")
 
-    def __init__(self, ) -> None:
+    def __init__(self,
+                 id: str,
+                 name: str,
+                 url: str,
+                 salary_from: Union[int, float, str],
+                 salary_to: Union[int, float, str],
+                 salary_mode: str,
+                 employer_name: str,
+                 employer_url: str,
+                 requirements: str,
+                 area: str) -> None:
+        "Конструктор для вакансии"
+        self.id = id
+        self.name = name
+        self.url = url
+        self.salary_from = self.get_salary_from(salary_from, salary_to, salary_mode)
+        self.salary_to = self.get_salary_to(salary_from, salary_to, salary_mode)
+        self.salary_mode = salary_mode
+        self.employer_name = employer_name
+        self.employer_url = employer_url
+        self.requirements = requirements
+        self.area = area
+
+    def get_salary_from(self, salary_from, salary_to, salary_mode):
+        "Возвращает нижнюю границу диапазона зарплаты"
         pass
+
+    def get_salary_to(self, salary_from, salary_to, salary_mode):
+        "Возвращает верхнюю границу диапазона зарплаты"
+        pass
+
+
+
+
