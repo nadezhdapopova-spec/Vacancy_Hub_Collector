d5346551412ed9e8fdeb7e3ab9a232eb44674e93
Nadezhda_Popova <nadezhdapopova13@yandex.ru>
2025-08-02 14:21:40 +0700

    modified src/class_vacancy.py

--- src/class_vacancy.py
+++ src/class_vacancy.py
@@ -5,12 +5,12 @@ from typing import Union, Any
 
 
 class Vacancy:
-    "Класс для создания вакансии"
-    __slots__ = ("id", "name", "url", "salary_from", "salary_to", "employer_name",
-                 "employer_url", "requirements", "area")
+    """Класс для создания вакансии"""
+    __slots__ = ("__vac_id", "__name", "__url", "__min_salary", "__max_salary", "__employer_name",
+                 "__employer_url", "__requirements", "__area")
 
     def __init__(self,
-                 id: str,
+                 vac_id: str,
                  name: str,
                  url: str,
                  salary_from: Union[int, float, str],
@@ -19,144 +19,112 @@ class Vacancy:
                  employer_url: str,
                  requirements: str,
                  area: str) -> None:
-        "Конструктор для вакансии"
-        self._id = id
-        self._name = name
-        self._url = url
-        self._min_salary = self.__validate_salary_from(salary_from)
-        self._max_salary = self.__validate_salary_to(salary_from, salary_to)
-        self._employer_name = employer_name
-        self._employer_url = employer_url
-        self._requirements = requirements
-        self._area = area if area else "не указано"
+        """Конструктор для вакансии"""
+        self.__vac_id = vac_id
+        self.__name = name
+        self.__url = url
+        self.__min_salary = self.__validate_salary_from(salary_from)
+        self.__max_salary = self.__validate_salary_to(salary_from, salary_to)
+        self.__employer_name = employer_name
+        self.__employer_url = employer_url
+        self.__requirements = requirements
+        self.__area = area if area else "не указано"
 
     def __str__(self) -> str:
         """Возвращает строковое представление вакансии для пользователя"""
-        return (f"{self._name}\nЗарплата: от {self._min_salary} до {self._max_salary}\n"
-                f"Компания: {self._employer_name}\nГород: {self._area}\n"
-                f"Ссылка на вакансию: {self._url}\nСсылка на компанию: {self._employer_url}\n")
+        return (f"{self.__name}\nЗарплата: от {self.__min_salary} до {self.__max_salary}\n"
+                f"Компания: {self.__employer_name}\nГород: {self.__area}\n"
+                f"Ссылка на вакансию: {self.__url}\nСсылка на компанию: {self.__employer_url}\n")
 
     def __lt__(self, other: Vacancy) -> bool:
-        self._check_comparability(other)
-        return self._salary_range() < other._salary_range()
+        """Сравнивает, является ли заработная плата в одной вакансии меньше, чем во второй"""
+        self.__check_comparability(other)
+        return self.salary_range < other.salary_range
 
     def __eq__(self, other: Vacancy) -> bool:
-        self._check_comparability(other)
-        return self._salary_range() == other._salary_range()
+        """Сравнивает, являются ли заработные платы двух вакансий одинаковыми"""
+        self.__check_comparability(other)
+        return self.salary_range == other.salary_range
 
     def __gt__(self, other: Vacancy) -> bool:
-        self._check_comparability(other)
-        return self._salary_range() > other._salary_range()
+        """Сравнивает, является ли заработная плата в одной вакансии больше, чем во второй"""
+        self.__check_comparability(other)
+        return self.salary_range > other.salary_range
 
     @property
-    def _id(self) -> str:
-        """Возвращает цену товара"""
-        return self._id
-
-    @_id.setter
-    def _id(self, value):
-        self._id = value
+    def vac_id(self) -> str:
+        """Возвращает id вакансии"""
+        return self.__vac_id
 
     @property
-    def _name(self) -> str:
-        """Возвращает цену товара"""
-        return self._name
-
-    @_name.setter
-    def _name(self, value):
-        self._name = value
+    def name(self) -> str:
+        """Возвращает наименование вакансии"""
+        return self.__name
 
     @property
-    def _url(self) -> str:
-        """Возвращает цену товара"""
-        return self._url
-
-    @_url.setter
-    def _url(self, value):
-        self._url = value
+    def url(self) -> str:
+        """Возвращает ссылку на вакансию"""
+        return self.__url
 
     @property
-    def _min_salary(self) -> int:
-        """Возвращает цену товара"""
-        return self._min_salary
-
-    @_min_salary.setter
-    def _min_salary(self, value):
-        self._min_salary = value
+    def min_salary(self) -> int:
+        """Возвращает нижнюю границу заработной платы"""
+        return self.__min_salary
 
     @property
-    def _max_salary(self) -> Union[int, float]:
-        """Возвращает цену товара"""
-        return self._max_salary
-
-    @_max_salary.setter
-    def _max_salary(self, value):
-        self._max_salary = value
+    def max_salary(self) -> Union[int, float]:
+        """Возвращает верхнюю границу заработной платы"""
+        return self.__max_salary
 
     @property
-    def _employer_name(self) -> str:
-        """Возвращает цену товара"""
-        return self._employer_name
-
-    @_employer_name.setter
-    def _employer_name(self, value):
-        self._employer_name = value
+    def employer_name(self) -> str:
+        """Возвращает наименование компании"""
+        return self.__employer_name
 
     @property
-    def _employer_url(self) -> str:
-        """Возвращает цену товара"""
-        return self._employer_url
-
-    @_employer_url.setter
-    def _employer_url(self, value):
-        self._employer_url = value
+    def employer_url(self) -> str:
+        """Возвращает ссылку на страницу компании"""
+        return self.__employer_url
 
     @property
-    def _requirements(self) -> str:
-        """Возвращает цену товара"""
-        return self._requirements
-
-    @_requirements.setter
-    def _requirements(self, value):
-        self._requirements = value
+    def requirements(self) -> str:
+        """Возвращает список требований в вакансии"""
+        return self.__requirements
 
     @property
-    def _area(self) -> str:
-        """Возвращает цену товара"""
-        return self._area
+    def area(self) -> str:
+        """Возвращает местоположение (город) в вакансии"""
+        return self.__area
 
-    @_area.setter
-    def _area(self, value):
-        self._area = value
-
-    def _salary_range(self) -> tuple:
-        return self._min_salary, self._max_salary
+    @property
+    def salary_range(self) -> tuple:
+        """Возвращает кортеж с нижней и верхней границами заработной платы"""
+        return self.__min_salary, self.__max_salary
 
     @staticmethod
     def __validate_salary_from(salary_from: Union[int, float, str]) -> int:
         """Проверяет валидность нижней границы заработной платы"""
-        if isinstance(salary_from, Union[int, float]):
+        if isinstance(salary_from, (int, float)):
             return int(salary_from)
         if isinstance(salary_from, str):
             if salary_from.isdigit():
                 return int(salary_from)
-        else:
-            return 0
+        return 0
 
     @staticmethod
     def __validate_salary_to(salary_from, salary_to: Union[int, float, str]) -> Union[int, float]:
         """Проверяет валидность верхней границы заработной платы"""
-        if isinstance(salary_to, Union[int, float]):
+        if isinstance(salary_to, (int, float)):
             return int(salary_to)
         if isinstance(salary_to, str):
             if salary_to.isdigit():
                 return int(salary_to)
         if salary_from and not salary_to:
             return sys.float_info.max
-        else:
-            return 0
+        return 0
 
     @staticmethod
-    def _check_comparability(other: Any) -> None:
+    def __check_comparability(other: Any) -> None:
+        """Проверяет принадлежность объекта к классу Vacancy"""
         if not isinstance(other, Vacancy):
             raise TypeError("Можно сравнивать объекты класса Vacancy")
